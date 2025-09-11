4f8d0d6dc880a20c1cf4f55646994afe5457dcda
cagla su -kachow <caglasu6@gmail.com>
2025-09-11 15:46:23 +0000

    margin

--- models/margin.sql
+++ models/margin.sql
@@ -8,5 +8,5 @@ SELECT
         ROUND((s.quantity * p.purchase_price), 2) AS purchase_cost,
         ROUND (s.revenue - (s.quantity * p.purchase_price), 2) AS margin
 FROM {{ref('stg_raw__sales')}} AS s
-JOIN {{ref('stg_raw__product')}} AS p
+LEFT JOIN {{ref('stg_raw__product')}} AS p
 USING (products_id)
